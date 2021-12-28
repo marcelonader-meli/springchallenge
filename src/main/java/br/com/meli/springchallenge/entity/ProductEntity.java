bc08e282a41cd05a38c78d6c5dc7f1d2ee150ed7
Felipe Dos Santos Conceição <felipe.conceicao@mercadolivre.com>
2021-12-28 15:57:25 -0300

    Criando estrutura basica do Product

---
+++ src/main/java/br/com/meli/springchallenge/entity/ProductEntity.java
@@ -0,0 +1,25 @@
+package br.com.meli.springchallenge.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductEntity {
+
+    private Long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private BigDecimal price;
+    private Integer quantity;
+    private Boolean freeShipping;
+    private String pretige;
+
+}
---
+++ src/main/java/br/com/meli/springchallenge/repository/ProductRepository.java
@@ -0,0 +1,33 @@
+package br.com.meli.springchallenge.repository;
+
+import br.com.meli.springchallenge.entity.ProductEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private List<ProductEntity> productList = new ArrayList<>();
+
+    public List<ProductEntity> listAll(){
+        return this.productList;
+    }
+
+    public ProductEntity findOneById(Long productId){
+        return productList.stream().filter(p -> p.getProductId() == productId).findFirst().orElse(new ProductEntity());
+    }
+
+    public void save(ProductEntity productEntity){
+        this.productList.add(productEntity);
+    }
+
+    public void removeById(Long productId){
+
+        ProductEntity productEntity = findOneById(productId);
+
+        this.productList.remove(productEntity);
+    }
+
+}
