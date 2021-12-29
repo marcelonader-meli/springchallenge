b16fe3b9f7ec14caa40c59a4366fee85cf90b1d3
gabielarocha21 <gabriela.rocha@mercadolivre.com>
2021-12-29 12:21:48 -0300

    adição do metodo salvar proutos e listar os produtos

--- src/main/java/br/com/meli/springchallenge/controller/ProductController.java
+++ src/main/java/br/com/meli/springchallenge/controller/ProductController.java
@@ -2,6 +2,8 @@ package br.com.meli.springchallenge.controller;
 
 import br.com.meli.springchallenge.entity.ProductEntity;
 import br.com.meli.springchallenge.entity.ShoppingCartEntity;
+import br.com.meli.springchallenge.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +12,41 @@ import java.util.List;
 @RestController
 @RequestMapping("articles")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
 
-    @GetMapping
-    public ResponseEntity<?> listAll(){
+    @GetMapping(value = "/")
+    public ResponseEntity<List<ProductEntity>> findAll() {
+        try {
+            return ResponseEntity.ok().body(productService.listAll());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @GetMapping(value = "/teste")
+    public ResponseEntity<?> listProductByCategory(@RequestParam String category) {
         return null;
     }
 
-    @GetMapping
-    public ResponseEntity<?> listProductByCategory(@RequestParam String category){
-        return null;
-    }
-
-    @GetMapping
-    public ResponseEntity<?> listProductsFiltered(@RequestParam String category, @RequestParam Boolean freeShipping, @RequestParam String brand, @RequestParam Double price, @RequestParam String prestige, @RequestParam Integer order){
+    @GetMapping(value = "/teste2")
+    public ResponseEntity<?> listProductsFiltered(@RequestParam String category, @RequestParam Boolean freeShipping, @RequestParam String brand, @RequestParam Double price, @RequestParam String prestige, @RequestParam Integer order) {
         return null;
     }
 
     @PostMapping("/purchase-request")
-    public ResponseEntity<?> buyProduct(@RequestBody ShoppingCartEntity shoppingCart){
+    public ResponseEntity<?> buyProduct(@RequestBody ShoppingCartEntity shoppingCart) {
         return null;
     }
 
     @PostMapping("/insert-articles-request")
-    public ResponseEntity<?> buyProduct(@RequestBody List<ProductEntity> listProducts) {
-        return null;
+    public ResponseEntity<List<ProductEntity>> saveProduct(@RequestBody List<ProductEntity> listProducts) {
+        try {
+            return ResponseEntity.ok().body(productService.saveProduct(listProducts));
+        }catch (Exception e ){
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
 }
--- src/main/java/br/com/meli/springchallenge/service/ProductService.java
+++ src/main/java/br/com/meli/springchallenge/service/ProductService.java
@@ -1,9 +1,24 @@
 package br.com.meli.springchallenge.service;
 
+import br.com.meli.springchallenge.entity.ProductEntity;
+import br.com.meli.springchallenge.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductEntity> listAll(){
+        return productRepository.listAll();
+    }
 
 
+    public List<ProductEntity> saveProduct(List<ProductEntity> products) {
+        productRepository.listAll().addAll(products);
+        return products;
+    }
 }
