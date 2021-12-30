fbb8cbfa04aa266814f48662145e8eaff083c1b8
gabielarocha21 <gabriela.rocha@mercadolivre.com>
2021-12-30 09:26:47 -0300

    correção ticketDTO

--- src/main/java/br/com/meli/springchallenge/DTO/TicketDTO.java
+++ src/main/java/br/com/meli/springchallenge/DTO/TicketDTO.java
@@ -15,8 +15,14 @@ import java.util.List;
 @AllArgsConstructor
 public class TicketDTO {
 
+    private static Long cont =1000L;
+
     private Long id;
     private List<ProductEntity>  articles;
     private BigDecimal total;
+
+    public static Long getCont(){
+        return cont++;
+    }
 
 }
